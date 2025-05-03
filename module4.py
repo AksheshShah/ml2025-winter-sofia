63480ca9d44d0713245e12e78966e84339908e5d
Akshesh Shah <aksheshshah@Aksheshs-MacBook-Air.local>
2025-05-03 10:04:24 -0700

    updating the code

--- module4.py
+++ module4.py
@@ -1,11 +1,17 @@
 # Ask the user for a positive integer N
 N = int(input("Enter a positive integer N: "))
 
+# Print the entered value
+print("You entered:", N)
+
 # Read N numbers one by one
 numbers = []
 for i in range(N):
     num = int(input("Enter number: "))
     numbers.append(num)
+
+# Print the list of numbers entered
+print("You entered:", numbers)
 
 # Ask the user for input X
 X = int(input("Enter the number to search for (X): "))
@@ -15,4 +21,4 @@ if X in numbers:
     index = numbers.index(X) + 1  # Convert to 1-based index
     print(index)
 else:
-    print(-1)
+    print("-1")
